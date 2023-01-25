c5c4113420acdf47a132fedf52477080b34eaf56
Юрий Кондращук <kondoryura@gmail.com>
2023-01-25 14:49:26 +0300

    Init

---
+++ src/main/java/ru/kondor256/spring5lessons/Spring5lessonsApplication.java
@@ -0,0 +1,13 @@
+package ru.kondor256.spring5lessons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5lessonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5lessonsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kondor256/spring5lessons/Spring5lessonsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kondor256.spring5lessons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5lessonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
